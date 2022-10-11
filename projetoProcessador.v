17917658f3ccaafd91c82746268bfd54c118b446
MontMor4 <victorperial@hotmail.com>
2022-10-11 01:50:21 -0300

    update

--- projetoProcessador.v
+++ projetoProcessador.v
@@ -2,7 +2,6 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 	input [4:0] DIN;
 	input Resetn, Clock, Run;
 	output Done;
-	//. . . declare variables
 	reg [15:0] IR;
 	wire [2:0] III;
 	wire IMM;
@@ -10,7 +9,7 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 	wire [2:0]rY;
 	reg rX_in,A_in; reg [2:0]Tstep_Q; reg [2:0]Tstep_D; reg[15:0]BusWires;
 	 
-	wire[15:0]r0,r7,r1,r2,r3,r4,r5,r6; //registradores de prop´sito geral
+	wire[15:0]r0,r7,r1,r2,r3,r4,r5,r6; //registradores de proposito geral
 	reg Done,IR_in;reg[3:0]Select;
 	wire [8:0]Imm;
 	wire [15:0]G;
@@ -18,8 +17,10 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 	reg G_in,AddSub;
 	wire[7:0] R_in;
 	wire [15:0] RA_out,RG_out;
-	wire [15:0] saidaALU,saidaROM,saidaIR;
-	reg espera;
+	wire [15:0] saidaALU, saidaROM, saidaIR, DadoMem;
+	reg [15:0] data;
+	reg wren;
+	reg addr_in;
 	
 	assign III = IR[15:13]; //opcode da instrução
 	assign IMM = IR[12]; //bit que indica se é imediato ou não
@@ -29,16 +30,17 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 	
 	dec3to8 decX (rX_in, rX, R_in); //habilita os registradores de propósito geral a receberem dados
 	
-	instr_memory memInstr(DIN,Clock,saidaROM); //modulo da memória de intruções
+	instr_memory memInstr(DIN, Clock, saidaROM); //modulo da memória de intruções
+	
+	memory memPrincipal(r7[7:0], Clock, data, wren, DadoMem); //módulo da memória principal
 	
 	//regn regIR(saidaROM,Resetn,IR_in,Clock,saidaIR);
 	
-	parameter T0 = 3'b000, T1 = 3'b001, T2 = 3'b010, T3 = 3'b011;
+	parameter T0 = 3'b000, T1 = 3'b001, T2 = 3'b010, T3 = 3'b011, T4 = 3'b100, T5 = 3'b101;
 	
 	initial begin
 		Tstep_Q = T0;
 		IR_in = 1'b0;
-		espera = 1'b1;
 	end
 	
 	// Control FSM state table
@@ -46,25 +48,37 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 	always @(Tstep_Q, Run, Done)//controla o funcionamento do processador
 		
 		case (Tstep_Q)
+			
 			T0: // data is loaded into IR in this time step
-			begin
 				if (~Run) Tstep_D = T0;
 				else Tstep_D = T1;
-			end
-			T1: if(Done)Tstep_D=T0;
-			else Tstep_D = T2;//. . .
 			
-			T2: if(Done)Tstep_D=T0;
-			else Tstep_D = T3;//. . .
+			T1: 
+				if(Done)Tstep_D = T0;
+				else Tstep_D = T2;
 			
-			T3:Tstep_D=T0;//. . .
-			//. . .
+			T2: 
+				if(Done)Tstep_D = T0;
+				else Tstep_D = T3;
+			
+			T3:
+				if(Done)Tstep_D = T0;
+				else Tstep_D = T4;
+			
+			T4:
+				if(Done)Tstep_D = T0;
+				else Tstep_D = T5;
+			
+			T5:
+				Tstep_D = T0;
+				
 		endcase
 		
 			
 		//parametros que indicam o opcode da instrução
-		parameter mv = 3'b000, mvt = 3'b001, add = 3'b010, sub = 3'b011;
-		// selectors for the BusWires multiplexer
+		parameter mv = 3'b000, mvt = 3'b001, add = 3'b010, sub = 3'b011, load = 3'b100, store = 3'b101, _and = 3'b110, branch = 3'b111;
+		
+		//selectors for the BusWires multiplexer
 		parameter _R0 = 4'b0000, _R1 = 4'b0001, _R2 = 4'b0010,
 			_R3 = 4'b0011, _R4 = 4'b0100,_R5 = 4'b0101,_R6 = 4'b110, _R7 = 4'b0111, _G = 4'b1000,
 			_IR8_IR8_0 /* signed-extended immediate data */ = 4'b1001,
@@ -215,7 +229,7 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 			
 			regn reg_6(BusWires, Resetn, R_in[1], Clock, r6);
 			
-			regn reg_7(BusWires, Resetn, R_in[0], Clock, r7);
+			pc_counter reg_7(BusWires, Resetn, R_in[0], Clock, r7);
 			
 			regn A(BusWires,Resetn,A_in ,Clock ,RA_out);
 			
@@ -264,30 +278,45 @@ module projetoProcessador(DIN, Resetn, Clock, Run, Done);
 					3'b111: Y = 8'b00000001;
 				endcase
 endmodule
+	
+	module pc_counter(clock, entrada_pc, pc_incr, pc_in, saida_pc);
+		input clock;
+		input [15:0] entrada_pc;
+		input pc_incr;
+		input pc_in;
+		output reg [15:0] saida_pc;
+				
+		always @(posedge clock)
+			
+			if(pc_incr && pc_in) begin
+				saida_pc = entrada_pc + 1'b1;
+			end	
+				
+	endmodule
+	
+	
 	module regn(BusW,Reset,R_IN,clock,R_OUT);
-	input [15:0]BusW;
-	input Reset;
-	input R_IN,clock;
-	output reg[15:0] R_OUT;
-	
-	always @(posedge clock) // antigo: negedge reset
-		if (R_IN) R_OUT <= BusW;
-	
+		input [15:0]BusW;
+		input Reset;
+		input R_IN,clock;
+		output reg[15:0] R_OUT;
+		
+		always @(posedge clock) // antigo: negedge reset
+			if (R_IN) R_OUT <= BusW;	
 	endmodule 
 	
 	
 	module Alu(BusW,RA_out,Addsub,Res);
-	input [15:0] BusW;
-	input [15:0] RA_out;
-	input Addsub;
-	output reg [15:0] Res;
+		input [15:0] BusW;
+		input [15:0] RA_out;
+		input Addsub;
+		output reg [15:0] Res;
 
-	always@(*)
-	if(!Addsub) begin 
-		Res <= BusW + RA_out;
-		end else begin
-			Res <= RA_out - BusW;
-		end
-	
+		always@(*)
+		if(!Addsub) begin 
+			Res <= BusW + RA_out;
+			end else begin
+				Res <= RA_out - BusW;
+			end
 	endmodule
 
